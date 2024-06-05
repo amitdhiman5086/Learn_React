64dc67fd86230be4aad9da23dd940954073d6fa9
Amit <amitdhiman5086@gmail.com>
2024-06-05 21:47:06 +0530

    Class 4

--- Code/Class 3/App.js
+++ Code/Class 3/App.js
@@ -15,33 +15,38 @@
 //Zero Configuration
 //Create Server
 
-
-
 //Transitive Dependencies
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Namste React 1"
-);
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "heading",
-  },
-  "Namste React ok changed 2"
-);
+// const heading1 = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//     key: "h2",
+//   },
+//   "Namste React 1"
+// );
+// console.log(heading1);
+// const heading2 = React.createElement(
+//   "h2",
+//   {
+//     id: "heading",
+//     key: "h1",
+//   },
+//   "Namste React ok changed 2"
+// );
+
+//New Syntax of Creating elements using JSX(JavsScript With XML(Extensible Markeup Language))
+const heading1 = (<h1>Namste JavaScript</h1>);
+
 const container = React.createElement(
   "h1",
   {
     id: "container",
   },
-  [heading1, heading2]
+  [heading1]
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ Notes/Assignment1/Class3.md
@@ -0,0 +1,1 @@
+## npx == npm run //means npm install the package and run executes it ,, npx execute the installed pacakge.
